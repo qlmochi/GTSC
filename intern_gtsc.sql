449047d6f853880825c6f7bc5271a6a6fb3bd535
qlmochi <xuanmai0212k@gmail.com>
2024-07-29 09:07:37 +0700

    init project

--- intern_gtsc.sql
+++ intern_gtsc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 29, 2024 lúc 03:14 AM
+-- Thời gian đã tạo: Th7 29, 2024 lúc 04:03 AM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
@@ -37,9 +37,7 @@ CREATE TABLE `danhmuc` (
 --
 
 INSERT INTO `danhmuc` (`id`, `name`) VALUES
-(5, 'điện thoạiipad'),
-(8, '123'),
-(9, 'ppp');
+(10, 'Điện thoại ');
 
 -- --------------------------------------------------------
 
@@ -55,31 +53,6 @@ CREATE TABLE `sanpham` (
   `mota` text DEFAULT NULL,
   `luotxem` int(11) DEFAULT 0,
   `iddm` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Đang đổ dữ liệu cho bảng `sanpham`
---
-
-INSERT INTO `sanpham` (`id`, `name`, `price`, `img`, `mota`, `luotxem`, `iddm`) VALUES
-(13, '22', 45.00, '3x4.jpg', 'xinh đẹp toẹt zời', 0, 5),
-(24, 'abc', 1000000.00, 'anh.jpg', 'aaaaaccccc', 0, 8),
-(29, 'abc', 1000000.00, 'payment.jpg', 'ac', 0, 5);
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `taikhoan`
---
-
-CREATE TABLE `taikhoan` (
-  `id` int(11) NOT NULL,
-  `user` varchar(50) NOT NULL,
-  `pass` varchar(50) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `tel` varchar(20) DEFAULT NULL,
-  `role` tinyint(4) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -100,13 +73,7 @@ CREATE TABLE `thanhvien` (
 --
 
 INSERT INTO `thanhvien` (`id`, `user`, `pass`, `level`) VALUES
-(1, 'qlcheese', '123', 2),
-(2, 'qlcheese', 'ác', 2),
-(3, 'admin', '1', 2),
-(4, 'xuanmai0212k@gmail.com', '123', 2),
-(5, 'wiener', '123', 2),
-(6, 'wiener', '123', 2),
-(7, 'wiener', '123', 2);
+(4, 'xuanmai0212k@gmail.com', '123', 2);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -126,12 +93,6 @@ ALTER TABLE `sanpham`
   ADD KEY `lk_sanpham_danhmuc` (`iddm`);
 
 --
--- Chỉ mục cho bảng `taikhoan`
---
-ALTER TABLE `taikhoan`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Chỉ mục cho bảng `thanhvien`
 --
 ALTER TABLE `thanhvien`
@@ -145,19 +106,13 @@ ALTER TABLE `thanhvien`
 -- AUTO_INCREMENT cho bảng `danhmuc`
 --
 ALTER TABLE `danhmuc`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `sanpham`
 --
 ALTER TABLE `sanpham`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
-
---
--- AUTO_INCREMENT cho bảng `taikhoan`
---
-ALTER TABLE `taikhoan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT cho bảng `thanhvien`
